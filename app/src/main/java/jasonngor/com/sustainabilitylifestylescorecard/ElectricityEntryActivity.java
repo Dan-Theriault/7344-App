bc4e0a3c629e4ce1f43323ead19c3386196fa784
dtrementozzi3 <dtremnz@gatech.edu>
2018-02-09 12:36:11 -0500

    added date entries to water and electricity bill pages

--- app/src/main/java/jasonngor/com/sustainabilitylifestylescorecard/ElectricityEntryActivity.java
+++ app/src/main/java/jasonngor/com/sustainabilitylifestylescorecard/ElectricityEntryActivity.java
@@ -14,27 +14,36 @@ import android.widget.Toast;
 public class ElectricityEntryActivity extends AppCompatActivity {
 
     public static EditText data;
+    public static EditText date;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_electricity_entry);
 
         data = (EditText) findViewById(R.id.editText);
+        date = (EditText) findViewById(R.id.enterDate);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String entries = data.getText().toString() + " kWh";
+                String entries = data.getText().toString() + " kWh        Date: ";
+                String dates = date.getText().toString();
+                entries = entries + dates;
 
-                if (entries == null || entries.equals(" kWh")) {
+                if (entries == null || entries.equals(" kWh        Date: ")) {
                     Toast.makeText(getBaseContext(), "Cannot submit empty field", Toast.LENGTH_LONG).show();
+                } else if (dates == null|| dates.equals("")) {
+                    Toast.makeText(getBaseContext(), "Please enter a date", Toast.LENGTH_LONG).show();
                 } else {
                     ElectricityActivity.list.add(entries);
                     ArrayAdapter<String> adapter = new ArrayAdapter<String>(ElectricityEntryActivity.this, android.R.layout.simple_list_item_1, ElectricityActivity.list);
                     ElectricityActivity.show.setAdapter(adapter);
-                    ((EditText)findViewById(R.id.editText)).setText("");
+                    ((EditText)findViewById(R.id.editText)).setText("Electricity Usage (kWh)");
+                    ((EditText)findViewById(R.id.enterDate)).setText("Date (mm/dd/yy)");
+                    Toast.makeText(getBaseContext(), "New entry added!", Toast.LENGTH_LONG).show();
                 }
+
             }
         });
     }
--- app/src/main/java/jasonngor/com/sustainabilitylifestylescorecard/WaterBillEntryActivity.java
+++ app/src/main/java/jasonngor/com/sustainabilitylifestylescorecard/WaterBillEntryActivity.java
@@ -1,5 +1,6 @@
 package jasonngor.com.sustainabilitylifestylescorecard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,27 +14,36 @@ import android.widget.Toast;
 public class WaterBillEntryActivity extends AppCompatActivity {
 
     public static EditText data;
+    public static EditText date;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_water_bill_entry);
         data = (EditText) findViewById(R.id.editText2);
+        date = (EditText) findViewById(R.id.enterwdate);
 
 
         FloatingActionButton waterButton = (FloatingActionButton) findViewById(R.id.waterButton);
         waterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String entries = data.getText().toString() + " Tgal";
+                String entries = data.getText().toString() + " Tgal        Date: ";
+                String dates = date.getText().toString();
+                entries = entries + dates;
 
-                if (entries == null || entries.equals(" Tgal")) {
+                if (entries == null || entries.equals(" Tgal        Date: ")) {
                     Toast.makeText(getBaseContext(), "Cannot submit empty field", Toast.LENGTH_LONG).show();
+                } else if (dates == null||dates.equals("")) {
+                    Toast.makeText(getBaseContext(), "Please enter a date", Toast.LENGTH_LONG).show();
                 } else {
                     WaterBillActivity.list.add(entries);
                     ArrayAdapter<String> adapter = new ArrayAdapter<String>(WaterBillEntryActivity.this, android.R.layout.simple_list_item_1, WaterBillActivity.list);
                     WaterBillActivity.show.setAdapter(adapter);
-                    ((EditText)findViewById(R.id.editText2)).setText("");
+                    ((EditText)findViewById(R.id.editText2)).setText("Water Usage (Tgal)");
+                    ((EditText)findViewById(R.id.enterwdate)).setText("Date (mm/dd/yy)");
+                    Toast.makeText(getBaseContext(), "New entry added!", Toast.LENGTH_LONG).show();
                 }
+
             }
         });
     }
