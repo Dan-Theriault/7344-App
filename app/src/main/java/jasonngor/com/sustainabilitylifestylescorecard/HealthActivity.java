9e7413beb73ec58ff0be72b48f49c5c4af7bb7c2
dianamilton <dmilton3@gatech.edu>
2018-04-12 15:35:53 -0400

    updated health

--- app/src/main/java/jasonngor/com/sustainabilitylifestylescorecard/HealthActivity.java
+++ app/src/main/java/jasonngor/com/sustainabilitylifestylescorecard/HealthActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 /**
  * Created by dianamilton on 3/29/18.
@@ -18,6 +20,7 @@ public class HealthActivity extends AppCompatActivity {
     public static EditText numCigs;
     public static CheckBox cigs;
     public static Button save;
+    public static Spinner clothesSpinner;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +28,14 @@ public class HealthActivity extends AppCompatActivity {
         cigsPerDayQ = (EditText) findViewById(R.id.editText3);
         numCigs = (EditText) findViewById(R.id.editText4);
         cigs = (CheckBox) findViewById(R.id.smokingBox);
+        //get the spinner from the xml.
+        clothesSpinner = (Spinner) findViewById(R.id.spinner);
+        //create a list of items for the spinner.
+        String[] items = new String[]{"Less than 33", "33 - 50", "51 - 74", "Greater than 75"};
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        //set the spinners adapter to the previously created one.
+        clothesSpinner.setAdapter(adapter);
         save = (Button) findViewById(R.id.save);
 
         save.setOnClickListener(new View.OnClickListener() {
